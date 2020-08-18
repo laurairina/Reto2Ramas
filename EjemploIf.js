f8dbe46bee99ec5f8a518355eca034af34436ed4
laura Irina <laurairina84@gmail.com>
2020-08-18 13:37:54 +0200

    subir cambios ramas

---
+++ EjemploIf.js
@@ -0,0 +1,39 @@
+/* var hour=17;
+var saludo="";
+
+if(hour<12){
+   saludo="Buenos días";
+
+}
+else if(hour<20){
+   saludo="Buenas tardes";
+
+}
+else{
+
+    saludo="Buenas noches";
+}
+console.log(saludo); */
+
+/* var corredores= ["maria","juan", "lucia"];
+var text="";
+for(var i=0; i<corredores.length; i++){
+   text += corredores[i]+"\n";
+}
+console.log(text);
+
+
+var index=0;
+var booleano= false;
+while(index< corredores.length && !booleano){
+   booleano= (corredores[index] == "lucia"); // solo para una condicion de parada porque devuelve un valor true o false
+  index++;
+}
+ */
+
+ //Funcion
+ function cubo(numero){
+    return numero*numero*numero;
+ }
+
+ console.log(cubo(5));
